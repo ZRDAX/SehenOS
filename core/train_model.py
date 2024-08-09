bbac0a060c822b7b949fe33c65fb6d3fd940d040
Bryan Rochadel <xadrez90@outlook.com>
2024-08-09 01:20:59 -0300

    tflite
    usando tflite

--- core/train_model.py
+++ core/train_model.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import layers, models
 
 # Carregar os dados pré-processados
 df_preprocessed = pd.read_csv('tables/preprocessed_data.csv')
@@ -11,27 +9,27 @@ df_preprocessed = pd.read_csv('tables/preprocessed_data.csv')
 features = df_preprocessed.drop(columns=['Intrusion']).values
 labels = (df_preprocessed['Intrusion'] != 'unknown').astype(int).values
 
-# Criar o modelo
-model = Sequential([
-    Dense(64, activation='relu', input_shape=(features.shape[1],)),
-    Dense(32, activation='relu'),
-    Dense(1, activation='sigmoid')
+# Definir o modelo
+model = models.Sequential([
+    layers.Dense(64, activation='relu', input_shape=(features.shape[1],)),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
 ])
 
 # Compilar o modelo
-model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Treinar o modelo
-model.fit(features, labels, epochs=20, batch_size=32, shuffle=True)
+model.fit(features, labels, epochs=20, batch_size=32)
 
 # Avaliar o modelo
-loss, accuracy = model.evaluate(features, labels)
+accuracy = model.evaluate(features, labels)[1]
 print(f'Accuracy: {accuracy:.4f}')
 
-# Converter para TensorFlow Lite
+# Converter para TFLite
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
-# Salvar o modelo convertido
+# Salvar o modelo TFLite
 with open('models/anomaly_detection_model.tflite', 'wb') as f:
     f.write(tflite_model)
