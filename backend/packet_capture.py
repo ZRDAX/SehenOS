2d157a403aea4f9e3cb6b65a20f944a3b4ef62c4
Bryan Rochadel <xadrez90@outlook.com>
2024-08-15 19:17:33 -0300

    capture print status
    print com status de execução.

--- backend/packet_capture.py
+++ backend/packet_capture.py
@@ -11,19 +11,25 @@ def connect_db():
             password="piswos",
             host="localhost"  # Nome do serviço no Docker Compose
         )
-        return conn
+        print("Connect to db: ", conn.get_dsn_parameters())
+        print('Conexão estabelecida com sucesso.')
     except Exception as e:
         print(f"Erro ao conectar ao banco de dados: {e}")
         return None
 
 # Função para extrair IP, hostname e FQDN usando RegEx
 def extract_info(packet):
-    src_ip = packet[IP].src if packet.haslayer(IP) else None
-    dst_ip = packet[IP].dst if packet.haslayer(IP) else None
+    src_ip = packet[0][1].src if packet.haslayer("IP") else None
+    dst_ip = packet[0][1].dst if packet.haslayer("IP") else None
     hostname_regex = r'^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'  # RegEx simplificada para FQDN
 
-    src_hostname = re.search(hostname_regex, src_ip).group(0) if src_ip and re.search(hostname_regex, src_ip) else None
-    dst_hostname = re.search(hostname_regex, dst_ip).group(0) if dst_ip and re.search(hostname_regex, dst_ip) else None
+    if src_ip:
+        src_hostname = re.search(hostname_regex, src_ip)
+        src_hostname = src_hostname.group(0) if src_hostname else None
+
+    if dst_ip:
+        dst_hostname = re.search(hostname_regex, dst_ip)
+        dst_hostname = dst_hostname.group(0) if dst_hostname else None
 
     return src_ip, src_hostname, dst_ip, dst_hostname
 
@@ -37,12 +43,15 @@ def insert_packet_data(conn, src_ip, src_hostname, dst_ip, dst_hostname):
         """, (src_ip, src_hostname, dst_ip, dst_hostname))
         conn.commit()
         cur.close()
+        print(f"Dados inseridos: src_ip={src_ip}, src_hostname={src_hostname}, dst_ip={dst_ip}, dst_hostname={dst_hostname}")
     except psycopg2.Error as e:
-        print(f"/problema-atual/ Erro ao inserir dados no banco de dados: {e.pgcode} - {e.pgerror}")
+        print(f" /problema-atual/ Erro ao inserir dados no banco de dados: {e.pgcode} - {e.pgerror}")
+        conn.rollback()
 
 # Função principal de callback para a captura de pacotes
 def packet_callback(packet):
     src_ip, src_hostname, dst_ip, dst_hostname = extract_info(packet)
+    print("Dados capturados:", src_ip, src_hostname, dst_ip, dst_hostname)
     if conn:
         insert_packet_data(conn, src_ip, src_hostname, dst_ip, dst_hostname)
 
@@ -51,8 +60,11 @@ def start_sniffing():
     global conn
     conn = connect_db()
     if conn:
-        sniff(prn=packet_callback, store=0, iface="eth0", promisc=True)
+        print('Iniciando a captura de pacotes...')
+        sniff(prn=packet_callback, filter="ip", store=0, iface='eth0', promisc=True)
         conn.close()
+    else:
+        print('Não foi possivel iniciar a captura de pacotes devido à falha na conexão com o banco de dados.')
 
 if __name__ == "__main__":
     start_sniffing()
